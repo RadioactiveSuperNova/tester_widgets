1b35c74c1ec3afb27f698dd694c2496c4201bc46
RadioactiveSuperNova <65427304+RadioactiveSuperNova@users.noreply.github.com>
2022-02-07 13:45:03 -0600

    Update Time dart

--- lib/time.dart
+++ lib/time.dart
@@ -1,5 +1,5 @@
 // Pull json data from WorldTimeAPI.org
 // https://worldtimeapi.org/api/timezone/America/Chicago
-// Store variable data to name
-//On ElevatedButton Press print to console
-// Change Button Text to TIME/DATE Retrieved onpress.
+// Store variable string data to namedlocation.
+//On ElevatedButton Press get & print to console
+// Change Button Text to $TIME/DATE Retrieved onpress.
