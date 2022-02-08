af39ebe3fd5ab709c5c0c2d42aacde52ce336e3b
RadioactiveSuperNova <65427304+RadioactiveSuperNova@users.noreply.github.com>
2022-02-08 14:25:53 -0600

    attempt at json

--- lib/main.dart
+++ lib/main.dart
@@ -57,30 +57,23 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Row(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            ElevatedButton(onPressed:(){}, child: Text('Button')),
-            ],
-        ),
-      ),
+        Column(
+        child:
+        FutureBuilder<Album>(
+          future: futureAlbum,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Text(snapshot.data!.title);
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },),
+        child:
+        ElevatedButton(onPressed:(){}, child: Text('Button')),
+      ),),
+    ),
       // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
--- lib/time.dart
+++ lib/time.dart
@@ -3,3 +3,97 @@
 // Store variable string data to namedlocation.
 //On ElevatedButton Press get & print to console
 // Change Button Text to $TIME/DATE Retrieved onpress.
+
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+Future<Album> fetchAlbum() async {
+  final response = await http
+      .get(Uri.parse('https://worldtimeapi.org/api/timezone/America/Chicago'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+class Album {
+  final int abbreviation;
+  final int client_id;
+  final String datetime;
+  final int day_of_week;
+  final int day_of_year;
+  final bool dst;
+  final String dst_from;
+  final int dst_offset;
+  final int dst_until;
+  final int raw_offset;
+  final String timezone;
+  final int unixtime;
+  final int utc_datetime;
+  final int utc_offset;
+  final int week_number;
+
+  const Album({
+    required this.abbreviation,
+    required this.client_id,
+    required this.datetime,
+    required this.day_of_week,
+    required this.day_of_year,
+    required this.dst,
+    required this.dst_from,
+    required this.dst_offset,
+    required this.dst_until,
+    required this.raw_offset,
+    required this.timezone,
+    required this.unixtime,
+    required this.utc_datetime,
+    required this.utc_offset,
+    required this.week_number,
+  });
+
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
+      abbreviation: json['abbreviation'],
+      client_id: json['client_id'],
+      datetime: json['datetime'],
+      day_of_week: json['day_of_week'],
+      day_of_year: json['day_of_year'],
+      dst: json['dst'],
+      dst_from: json['dst_from'],
+      dst_offset: json['dst_offset'],
+      dst_until: json['dst_until'],
+      raw_offset: json['raw_offset'],
+      timezone: json['timezone'],
+      unixtime: json['unixtime'],
+      utc_datetime: json['utc_datetime'],
+      utc_offset: json['utc_offset'],
+      week_number: json['week_number'],
+
+    );
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<Album> futureAlbum;
+
+  @override
+  void initState() {
+    super.initState();
+    futureAlbum = fetchAlbum();
+  }
+// ···
+}
+
+Future<http.Response> fetchAlbum() {
+  return http.get(Uri.parse('https://worldtimeapi.org/api/timezone/America/Chicago'));
+}
